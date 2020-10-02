202bfb93c5b8f5b846fbc58e71d152634bf6b5c7
olerie <71316118+olerie@users.noreply.github.com>
2020-10-02 13:00:58 +0200

    Update Taster_als_Schalter.ino
    Bibliothek eingebunden

--- Taster_als_Schalter/Taster_als_Schalter.ino
+++ Taster_als_Schalter/Taster_als_Schalter.ino
@@ -1,9 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <OneButton.h>
+OneButton taster(10, true);
+#define LED 13
+boolean statusLed;
+
+
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+
+  taster.attachClick(Funktion_Taster);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  taster.tick();
 
+  delay(10);
+}
+
+void Funktion_Taster()
+{
+  {
+    if (statusLed == HIGH)
+    {
+      digitalWrite(LED, LOW);
+      statusLed = 0;
+    }
+    else
+    {
+      digitalWrite(LED, HIGH);
+      statusLed = 1;
+    }
+  }
 }
